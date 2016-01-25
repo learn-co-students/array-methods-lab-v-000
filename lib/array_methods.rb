f48d42dc2e058059e4719582a814d1e450269990
rafeeq-123 <rafeeq-123@hotmail.com>
2016-01-24 17:45:21 -0800

    arrays

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array=["wow","blueberrys","sunshine","and","I","new","future"]
+array.sort
 end
 
 def using_reverse(array)
-
+array=["wow","to","get","my","head","arrays!"]
+array.reverse
 end
 
 def using_first(array)
+  array=["wow","now","mad","cow"]
+  array.first
 
 end
 
 def using_last(array)
-
+array=["love","doing","arrays!"]
+array.last
 end
 
 def using_size(array)
-
+array=["hey","hey","hey","world","you","crazy"]
+array.size
 end
