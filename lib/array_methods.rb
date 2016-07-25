208c6515e3c0481b4d9f295f71cf911b9c1c3bef
Sabrina Smith <stsmith84@msn.com>
2016-07-25 00:55:06 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
-def using_include(array, element)
-
+def using_include(numbers, one)
+numbers.include?(one)
 end
 
-def using_sort(array)
-
+def using_sort(numbers)
+numbers.sort
 end
 
-def using_reverse(array)
-
+def using_reverse(numbers)
+numbers.reverse
 end
 
-def using_first(array)
-
+def using_first(numbers)
+numbers.first
 end
 
-def using_last(array)
-
+def using_last(numbers)
+numbers.last
 end
 
-def using_size(array)
-
+def using_size(numbers)
+numbers.size
 end
