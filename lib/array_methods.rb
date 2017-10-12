68a00a696b89972b39c811f8fffc88e0bd3622a7
Tabitha Sin <tabitha.sin@gmail.com>
2017-10-12 19:33:33 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array = ["I", "am", "wow"]
+  array.sort
 end
 
 def using_reverse(array)
-
+  array = ["wow", "arrays!"]
+  array.reverse
 end
 
 def using_first(array)
-
+  array = ["wow", "arrays!"]
+  array.first
 end
 
 def using_last(array)
-
+  array = ["wow", "arrays!"]
+  array.last
 end
 
 def using_size(array)
-
+  array = ["aang", "katara", "sokka", "toph", "zuko", "suki"]
+  array.size
 end
