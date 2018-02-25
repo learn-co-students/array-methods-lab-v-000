4347a1c716452c2935523ba761fd52670e031fda
Kristin Reddington <kristinreddington@gmail.com>
2018-02-25 01:12:08 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,26 @@
 def using_include(array, element)
 
+array.include?(element)
 end
 
 def using_sort(array)
-
+array.sort
 end
 
 def using_reverse(array)
-
+array.reverse
 end
 
 def using_first(array)
-
+array = ["wow", "Patriots", "Eagles", "arrays!"]
+array.first
 end
 
 def using_last(array)
-
+  array = ["wow", "Patriots", "Eagles", "arrays!"]
+  array.last
 end
 
 def using_size(array)
-
+array.size
 end
