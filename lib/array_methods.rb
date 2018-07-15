2e4148a0bba0e254e9aac9e51ceaeeb337e5e355
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-15 08:57:04 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+  if array.include?(element)
+    return true
+  else
+    return false
+  end
 end
 
 def using_sort(array)
-
+  sorted_array = array.sort
+  return sorted_array
 end
 
 def using_reverse(array)
-
+  return array.reverse
 end
 
 def using_first(array)
-
+  return array[0]
 end
 
 def using_last(array)
-
+  return array.last
 end
 
 def using_size(array)
-
+  return array.length
 end
