7c6fc097f5cd260fd498d9b889713fe64ce16161
Hieu <github email address>
2018-12-01 18:22:37 +0430

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,11 @@
-def using_include(array, element)
+def using_include(array, element) ; array.include?(element) ;end
 
-end
+def using_sort(array) ; array.sort ; end
 
-def using_sort(array)
+def using_reverse(array) ; array.reverse ; end
 
-end
+def using_first(array) ; array.first ; end
 
-def using_reverse(array)
+def using_last(array) ; array.last ; end
 
-end
-
-def using_first(array)
-
-end
-
-def using_last(array)
-
-end
-
-def using_size(array)
-
-end
+def using_size(array) ; array.size ; end
