1bdf7623aa1b4fb56c05333aefbd4b71b440a9a7
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-20 17:36:42 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,11 +3,11 @@ def using_include(array, element)
 end
 
 def using_sort(array)
- array.sort 
+ array.sort
 end
 
 def using_reverse(array)
-  array.reverse 
+  array.reverse
 end
 
 def using_first(array)
@@ -19,5 +19,5 @@ def using_last(array)
 end
 
 def using_size(array)
-  array.size
+ array.size
 end
