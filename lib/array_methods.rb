b3ee87efa65d597f180c7b491c22162be3040e66
Lluvia Frias <lluvi.frias@gmail.com>
2020-05-08 17:24:19 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,14 @@
 def using_include(array, element)
-  array.include?(element)
+  if array.include?(element)
+    return true
+  else
+    return false
+  end
 end
 
 def using_sort(array)
-  array.sort
+  #sorted_array = array.sort
+  array.sort!
 end
 
 def using_reverse(array)
@@ -16,8 +21,41 @@ end
 
 def using_last(array)
   array.last
+
 end
 
 def using_size(array)
   array.size
+
 end
+
+
+
+
+
+
+
+
+# def using_include(array, element)
+#   array.include?(element)
+# end
+#
+# def using_sort(array)
+#   array.sort
+# end
+#
+# def using_reverse(array)
+#   array.reverse
+# end
+#
+# def using_first(array)
+#   array.first
+# end
+#
+# def using_last(array)
+#   array.last
+# end
+#
+# def using_size(array)
+#   array.size
+# end
